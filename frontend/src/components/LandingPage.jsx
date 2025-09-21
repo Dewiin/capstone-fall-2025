c9518d0c464106fd55772d333fd8e2a3b80cf26d
Devin Xie <devinxie418@gmail.com>
2025-09-20 22:09:14 -0400

    edit landing page and 404 page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,26 +1,16 @@
-import { Link } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
 import { Navbar01 } from './ui/shadcn-io/navbar-01';
 
 export function LandingPage() {
-    const darkMode = useRef(true);
-
-    useEffect(() => {
-        document.documentElement.classList.toggle("dark", darkMode);
-    }, [darkMode]); 
-
     return (
         <>
             <div className="relative w-full">
                 <Navbar01 />
             </div>
-            <ul>
-                <li>
-                    <Link to="/account">
-                        Account Profile
-                    </Link>
-                </li>
-            </ul>
+            <div className='flex justify-center align-center p-16 text-9xl text-center'>
+                <h1>
+                    Home Page
+                </h1>
+            </div>
         </>
     );
 } 
--- frontend/src/components/PageNotFound.jsx
+++ frontend/src/components/PageNotFound.jsx
@@ -1,30 +1,16 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Navbar01 } from "./ui/shadcn-io/navbar-01";
 
 export function PageNotFound() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+            <div className="relative w-full">
+                <Navbar01 />
             </div>
-            <h1>404 Page Not Found</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
+            <div className='flex justify-center align-center p-16 text-9xl text-center'>
+                <h1>
+                    404 Page Not Found :(
+                </h1>
             </div>
-            <Link to="/">
-                Back to Home
-            </Link>
         </>
     );
 } 
