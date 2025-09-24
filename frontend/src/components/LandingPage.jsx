f3e4186aaae1554d382fd987032f19d21740a665
Devin Xie <devinxie418@gmail.com>
2025-09-23 21:32:19 -0400

    Add names to deck model

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,4 +1,3 @@
-import { Navbar01 } from './ui/shadcn-io/navbar-01';
 import { useAuth } from './contexts/Contexts';
 
 export function LandingPage() {
@@ -8,7 +7,7 @@ export function LandingPage() {
         <>
             <div className='flex flex-col justify-center align-center p-16 text-center gap-10'>
                 <h1 className='text-9xl'>
-                    Home Page <br></br>
+                    Landing Page <br></br>
                 </h1>
                 <h3 className='text-5xl'>
                     {user ? `Welcome, ${user.displayName}` : "Not Logged In"}
--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router-dom";
 import { LoginForm } from "./LoginForm"; 
 import { SignupForm } from "./SignupForm";
-import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
 
 export function Form() {
     const { method } = useParams();
