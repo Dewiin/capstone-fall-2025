97e49c08226ab9cf593877891032756552385aaf
Devin Xie <devinxie418@gmail.com>
2025-10-07 20:03:25 -0400

    feat: refined gemini prompt to make reasonable quiz options, and added pdf input with gemini file api

--- backend/controllers/generateController.js
+++ backend/controllers/generateController.js
@@ -8,10 +8,8 @@ async function generateTextPost(req, res) {
         const { studySetName } = req.body;
        
         const deckResult = await gemini.textInputDeck(text);
-        const quizResult = await gemini.textInputQuiz(text);
-        // console.log(quizResult);
-
-        // console.log(result);
+        const deckObject = JSON.stringify(deckResult);
+        const quizResult = await gemini.generateQuiz(deckObject);
 
         if(deckResult.status == 1 && quizResult.status == 1) {
             // create study set
@@ -65,11 +63,9 @@ async function generateTextPost(req, res) {
                 studySetId: studySet.id,
             });
         }
-        else {
-            return res.json({
-                status: 0,
-            });
-        }
+        return res.json({
+            status: 0,
+        });
     } catch (err) {
         console.error(`Error posting text data for generating: `, err);
     }
@@ -77,10 +73,68 @@ async function generateTextPost(req, res) {
 
 async function generateFilePost(req, res) {
     try {
-        const buffer = req.file.buffer;
+        const pdfData = req.file;
+        const user = req.user
+        const { studySetName } = req.body;
 
+        const deckResult = await gemini.pdfInputDeck(pdfData);
+        const deckObject = JSON.stringify(deckResult);
+        const quizResult = await gemini.generateQuiz(deckObject);
+
+        if(deckResult.status == 1 && quizResult.status == 1) {
+            // create study set
+            const studySet = await prisma.studySet.create({
+                data: {
+                    name: studySetName,
+                    userId: user.id,
+                }
+            });
+
+            // create deck belonging to -> study set
+            const deck = await prisma.deck.create({
+                data: {
+                    studySetId: studySet.id,
+                }
+            });
+
+            // create each flash card belonging to -> deck
+            deckResult.flashCards.forEach( async (flashCard) => {
+                await prisma.flashcard.create({
+                    data: {
+                        question: flashCard.cardFront,
+                        answer: flashCard.cardBack,
+                        deckId: deck.id,
+                    }
+                });
+            });
+
+            // create quiz belonging to -> user
+            const quiz = await prisma.quiz.create({
+                data: {
+                    studySetId: studySet.id,
+                    highScore: 0,
+                }
+            });
+
+            // create quizQuestions belonging to -> quiz
+            quizResult.quiz.forEach(async (question) => {
+                await prisma.quizQuestion.create({
+                    data: {
+                        question: question.quizQuestion,
+                        choices: question.quizOptions,
+                        correctAnswer: question.answer,
+                        quizId: quiz.id,
+                    } 
+                });
+            });
+
+            return res.json({
+                status: 1,
+                studySetId: studySet.id,
+            });
+        }
         return res.json({
-
+            status: 0,
         });
     } catch (err) {
         console.error(`Error posting file data for generating: `, err);
