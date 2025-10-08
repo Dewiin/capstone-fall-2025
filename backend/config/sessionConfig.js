444e993ae7db234a5d795815b201c0e06b0457e8
Devin Xie <devinxie418@gmail.com>
2025-10-08 13:01:20 -0400

    fixed loading overlay, edit session config for development and production mode

--- backend/config/sessionConfig.js
+++ backend/config/sessionConfig.js
@@ -3,9 +3,13 @@ import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { prisma } from "./prismaClient.js";
 import "dotenv/config";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const sessionConfig = expressSession({
 	cookie: {
 		maxAge: 7 * 24 * 60 * 60 * 1000, 
+		sameSite: isProduction ? "none" : "lax",
+		secure: isProduction,
 	},
 	secret: process.env.SESSION_SECRET,
 	resave: false,
