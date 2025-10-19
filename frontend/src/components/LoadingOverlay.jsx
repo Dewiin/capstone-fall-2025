d29fb9a2fac6cb2a8f9430714f7b3c57ef359f23
Devin Xie <devinxie418@gmail.com>
2025-10-19 00:27:39 -0400

    chore: dark mode persistence

--- frontend/src/components/LoadingOverlay.jsx
+++ frontend/src/components/LoadingOverlay.jsx
@@ -4,10 +4,10 @@ import { cn } from "@/lib/utils";
 export function LoadingOverlay( {className, fixed = false} ) {
   return (
     <div 
-        className={cn(
-            `${fixed ? "fixed" : "absolute"} inset-0 flex items-center justify-center backdrop-blur-[3px] z-100 rounded-xl`,
-            className
-        )}
+      className={cn(
+        `${fixed ? "fixed" : "absolute"} inset-0 flex items-center justify-center backdrop-blur-[3px] z-100 rounded-xl`,
+        className
+      )}
     >
       <Spinner variant="default" size={24}></Spinner>
     </div>
