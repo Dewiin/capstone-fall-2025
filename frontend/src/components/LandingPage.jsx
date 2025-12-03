14fdbfc6560e7359a80a5255bc2b3761c3d78c28
Devin Xie <devinxie418@gmail.com>
2025-12-03 11:40:52 -0500

    add border radius to landing page banner

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -170,7 +170,7 @@ export function LandingPage() {
       <img 
         src={landingPageImage} 
         alt="Brainstorm banner"
-        className="w-full h-auto object-cover"
+        className="w-full h-auto object-cover rounded-2xl"
       />
     </section>
 
